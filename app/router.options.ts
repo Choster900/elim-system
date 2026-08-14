ca12523868ff964c2621b8d35f1e542a5551693e
Sergio Lopez <slopez@ditobanx.com>
2026-08-14 10:37:07 -0600

    feat: add memberRoutes to router configuration

--- app/router.options.ts
+++ app/router.options.ts
@@ -2,6 +2,7 @@ import type { RouterConfig } from '@nuxt/schema'
 import authRoutes from '~/presentation/auth/router.index'
 import dashboardRoutes from '~/presentation/dashboard/router.index'
 import landingRoutes from '~/presentation/landing/router.index'
+import memberRoutes from '~/presentation/members/router.index'
 import meetingsRoutes from '~/presentation/meetings/router.index'
 import registerRoutes from '~/presentation/register/router.index'
 import territoriesRoutes from '~/presentation/territories/router.index'
@@ -12,6 +13,7 @@ export default <RouterConfig>{
         ...authRoutes,
         ...registerRoutes,
         ...dashboardRoutes,
+        ...memberRoutes,
         ...meetingsRoutes,
         ...territoriesRoutes,
     ],
